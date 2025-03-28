56b03213ee64b752d3c4120a3b7f19bc23580de6
datdevs <datdevsx@gmail.com>
2025-03-28 23:21:02 +0700

    Update browser.ts to configure service worker URL based on document baseURI

--- src/backend/mocks/browser.ts
+++ src/backend/mocks/browser.ts
@@ -1,8 +1,14 @@
 import { setupWorker } from 'msw/browser';
 import { getData } from './real-estate';
 
+const basePath = document.baseURI || '/';
+
 const worker = setupWorker(...[getData]);
 
 export function enableMocking() {
-  return worker.start();
+  return worker.start({
+    serviceWorker: {
+      url: `${basePath}mockServiceWorker.js`,
+    },
+  });
 }
